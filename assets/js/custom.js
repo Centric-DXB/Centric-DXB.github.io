cc2dee84d0e4e9d64930ebc55fc4503e2191a7ce
Adnan Iqbal <adnan.iqbal@centric.ae>
2023-04-18 23:05:11 +0500

    Careers page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -635,7 +635,50 @@
     });
 
     
-
+    $('.careers-carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplaySpeed: 3000,
+        speed: 1000,
+        dots: true,
+        //autoplay: true,
+        arrows: false,
+        responsive: [
+            {
+            breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    //dots:true
+                }
+            },
+            {
+            breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    //dots:true
+                }
+            },
+            {
+            breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    //infinite: true,
+                    //dots: true
+                }
+            },
+            {
+            breakpoint: 550,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    //dots:true
+                }
+            },
+        ]
+    });
 
 
     
